9bf160592cc63ea1d26f78b80ef1ef3de9994598
ahr1384 <amirhoseinrahimi.work@gmail.com>
2021-09-17 22:55:53 +0430

    fix bugs & new data

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 
-// require('functions.php');
+require('functions.php');
 
-$i = true;
-
-if ($i === true){
-    header('Location: http://localhost/php/php-projects/login-register-form/login');
-}
+header('Location: login/?error=1');
--- login/index.php
+++ login/index.php
@@ -11,16 +11,44 @@
 
 <?php
 
+$error = '';
 
+if ($_SERVER["REQUEST_METHOD"] == 'GET') {
+
+    @$error = $_GET['error'];
+}
+
+
+switch ($error) {
+    case 1:
+        $display_error = 'Please enter your username and password';
+        break;
+
+    case 2:
+        $display_error = 'Please enter username and password';
+        break;
+
+    case 3:
+        $display_error = 'Your username is incorrect';
+        break;
+
+    case 4:
+        $display_error = 'Your password is incorrect';
+        break;
+
+    default:
+        $display_error = '';
+}
 
 ?>
 
 <body>
     <div class="main">
         <h1 class="sign">login & register</h1>
-        <form class="form1" action="../profile/index.php" method="post">
+        <form class="form1" action="../redirect.php" method="post">
             <input class="un" type="text" name="username" placeholder="Username" required>
-            <input class="pass" type="password" name="password" placeholder="Password">
+            <input class="pass" type="password" name="password" placeholder="Password" required>
+            <div class="error"><?php echo $display_error ?></div>
             <div class="btn-form">
                 <input class="submit" type="submit" name="type" value="login">
                 <input class="submit" type="submit" name="type" value="register">
